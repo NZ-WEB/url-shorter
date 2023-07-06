7eddf5d960e1a4de03818dde37d9a4900a428905
nzweb <nzweb@vk.com>
2023-07-06 14:33:54 +0300

    feat: init config and logger

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"golang.org/x/exp/slog"
+	"os"
+	"url-shortener/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shorter", slog.String("env", cfg.Env))
+	log.Debug("debut messages are enabled")
+
+	// TODO: init logger slog
+
+	// TODO: init storage sqlite
+
+	// TODO init router chi, render
+
+	// TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
